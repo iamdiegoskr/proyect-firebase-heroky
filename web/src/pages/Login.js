98f5da86d509dcfa46e09e0c6506ca2dfa33ddb5
Diego Martinez <juandiego337@hotmail.com>
2021-10-20 21:27:33 -0500

    Show user the succes o error in login

--- web/src/pages/Login.js
+++ web/src/pages/Login.js
@@ -5,6 +5,7 @@ import "firebase/auth";
 import { login } from "../actions/authActions";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import Swal from 'sweetalert2'
 
 firebase.initializeApp({
     apiKey: "AIzaSyDym2qMWJBJ2JwcWp4bHIwF_Aqrf3bxYNs",
@@ -42,7 +43,17 @@ const Login = ({ dispatch }) => {
 
     const loginUser = (event) => {
         event.preventDefault()
-        return auth.signInWithEmailAndPassword(userData.email, userData.password);
+        return auth.signInWithEmailAndPassword(userData.email, userData.password)
+            .then(()=>{
+                Swal.fire('Bienvenido....')
+            })
+            .catch(()=>{
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Usuario o contraseña invalida'
+                })
+            })
     }
 
 

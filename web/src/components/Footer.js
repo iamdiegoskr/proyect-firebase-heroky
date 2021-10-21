399ce6c29c98eb6456c0ebfc087a971d547aa1b0
Diego Martinez <juandiego337@hotmail.com>
2021-10-21 09:52:34 -0500

    Improving the footer design

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -3,7 +3,33 @@ import React from 'react'
 export const Footer = () => {
     return (
         <footer>
-            <p>Derechos reservador por JuanDiegoInc.</p>
+            <div className="social-medias">
+                <ul>
+                    <li>
+                        <a href="#">
+                            <img src="github.png" alt=""/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="linkedin.png" alt=""/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="youtube.png" alt=""/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="twiter.png" alt=""/>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className="profile-creator">
+                <p>By Juan Diego Loaiza Martinez 🔥🔥🔥</p>
+            </div>
         </footer>
     )
 }

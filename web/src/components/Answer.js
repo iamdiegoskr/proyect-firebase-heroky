33c6c74c6ee4a10de76a4200bc43a1f6f0f2d620
Diego Martinez <juandiego337@hotmail.com>
2021-10-21 19:40:07 -0500

    Adding rich text in the answer and question fields

--- web/src/components/Answer.js
+++ web/src/components/Answer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Answer = ({ answer, userId, onDelete }) => (
   <aside className="answer">
-    <p>{answer.answer}</p>
-    {answer.userId === userId && <button className="btb-delete-answer" onClick={()=>{onDelete(answer.id)}}>Eliminar</button>}
+    <div dangerouslySetInnerHTML={{__html:answer.answer}} />
+    {answer.userId === userId && <button className="button" onClick={() => onDelete(answer.id)}>DELETE</button>}
   </aside>
 )

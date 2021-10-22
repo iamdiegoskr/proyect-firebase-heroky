95eae6dae81c107b1d46dd87f79d1e854ab44e11
Diego Martinez <juandiego337@hotmail.com>
2021-10-22 11:31:48 -0500

    Implemented search of questions

--- web/src/components/Question.js
+++ web/src/components/Question.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export const Question = ({ question, excerpt, onDelete }) => (
-  <article className={excerpt ? 'question-excerpt' : 'question'}>
+export const Question = ({ question, excerpt, onDelete, setcategorySearch }) => {
+
+  const handleCategorySearch = (e)=>{
+    setcategorySearch(question.category)
+  }
+
+  return <article className={excerpt ? 'question-excerpt' : 'question'}>
     <h2><div dangerouslySetInnerHTML={{__html:question.question}}/></h2>
-    <p>{question.category}  - <small>{question.type}</small></p>
+
+    {setcategorySearch?
+            <p className="categorySearch" onClick={handleCategorySearch}>{question.category}  - <small>{question.type}</small></p>:
+            <p>{question.category}  - <small>{question.type}</small></p>}
 
     {onDelete && (
       <button className="button-delete right" onClick={() => onDelete(question.id)}>ELIMINAR 🗑️</button>
@@ -15,4 +23,4 @@ export const Question = ({ question, excerpt, onDelete }) => (
       </Link>
     )}
   </article>
-)
+}
--- web/src/pages/QuestionsPage.js
+++ web/src/pages/QuestionsPage.js
@@ -1,24 +1,43 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 
 import { fetchQuestions } from '../actions/questionActions'
 import { Question } from '../components/Question'
+import { Link } from 'react-router-dom'
 
 const QuestionsPage = ({ dispatch, loading, questions, hasErrors }) => {
+
     useEffect(() => {
         dispatch(fetchQuestions())
     }, [dispatch])
+
+    const [search, setSearch] = useState('');
+    const [categorySearch, setcategorySearch] = useState('');
+
+    var questionFilteredCategory = questions.filter(question => question.category.toUpperCase().includes(categorySearch.toUpperCase()));
+
+    var questionsFilteredSearch = questionFilteredCategory.filter(question => question.question.toUpperCase().includes(search.toUpperCase()));
+
+    const goTOVariable = questionsFilteredSearch[0]?.id;
+
+    const handleSearch = (e)=>{
+        setSearch(e.target.value);
+    }
 
     const renderQuestions = () => {
         if (loading) return <div className="loading">Loading questions...</div>
         if (hasErrors) return <p>Unable to display questions.</p>
 
-        return questions.map(question => <Question key={question.id} question={question} excerpt />)
+        return questionsFilteredSearch.map(question => <Question setcategorySearch={setcategorySearch} key={question.id} question={question} excerpt />)
     }
 
     return (
         <section>
             <h1>Questions</h1>
+            <form className="form-search">
+                <input type="text" onChange={handleSearch} placeholder="Buscar......."/>
+                <Link to={`/question/${goTOVariable}`}><input style={{display: 'none'}} type="submit" value="search" /></Link>
+            </form>
             {renderQuestions()}
         </section>
     )
